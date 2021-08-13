9810a179f4fc6f873f9f1fefd3cb44d74479926d
baijuka <baijuka@yahoo.com>
2021-08-13 22:27:33 +0000

    Add Logut functionality

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def login():
             # ensure hashed password matches user input
 
             if check_password_hash(existing_user["password"], request.form.get("password")):
-                session["users"] = request.form.get("username").lower()
+                session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
                 return redirect(url_for("profile",username=session["user"]))
             else:
@@ -77,10 +77,22 @@ def login():
 
 @app.route("/profile/<username>", methods = ["GET","POST"])
 def profile(username):
+    # grab the session user's username from db
     username = mongo.db.users.find_one(
-        {"username": session["users"]})["username"]
-    return render_template("profile.html",username=username)
+        {"username": session["user"]})["username"]
+    
+    if session["user"]:
+        return render_template("profile.html", username=username)
 
+    return redirect(url_for('login'))
+
+
+@app.route("/logout")
+def logout():
+    #remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
